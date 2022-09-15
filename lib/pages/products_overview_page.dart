fc989f7ac6c665c8632f19d4853b22aa0bafe272
Gustavo <kalungagus@yahoo.com>
2022-09-14 22:32:01 -0300

    Primeiro commit do projeto, para fazer upload dos arquivos no estado atual para o git

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
+import '../components/product_grid.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
+
+class ProductsOverviewPage extends StatelessWidget {
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorite,
+                child: Text('Somente favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
+      ),
+      body: ProductGrid(),
+    );
+  }
+}
